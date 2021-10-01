95f545a9c6a71ff1c8cbf0a099165ecd8783c6f9
cxmarcelo <marcelo.c.b22@gmail.com>
2021-10-01 00:18:04 -0300

    atualização password limitado.

--- app/models/users.py
+++ app/models/users.py
@@ -5,7 +5,7 @@ from app import db, ma
 class Users(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     email = db.Column(db.String(100), unique=True, nullable=False)
-    password = db.Column(db.String(20), nullable=False)
+    password = db.Column(db.String, nullable=False)
     crm = db.Column(db.String(20))
     name = db.Column(db.String(100), nullable=False)
     user_type = db.Column(db.Integer, nullable=False)
